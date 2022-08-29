0717a37eca677abf50ff22e15243113cde015dcf
Diego Urbina <diego.urbina14774@est.uca.edu.ni>
2022-08-29 09:53:40 -0600

    Agregue un ejercicio de arreglos

---
+++ Arreglo1.py
@@ -0,0 +1,16 @@
+numeros = []
+numeros.append(3)
+numeros.append(8)
+numeros.append(9)
+numeros.append(20)
+
+print(numeros)
+
+numeros.insert(1, 15)
+
+print(numeros)
+
+numeros.remove(15)
+
+print(numeros)
+
